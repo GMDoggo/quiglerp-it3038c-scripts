d2482f0b2daea1d07239a8ea273e28758a3a700b
GMDoggo <quiglerp@gmail.com>
2023-10-01 11:06:19 -0400

    Update Lab5.py
    Number Guessing Game

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,1 +1,27 @@
+#Guessing Game
+print("Guess the Number between 1 and 100")
+
+
+#Generate a random integer from 1-100
+import random
+
+random_number = (random.randint(1,100))
+
+#Start the loop
+
+while True:
+    
+    #Prompts the user for a guess
+    guess = int(input("Enter you guess: "))
+
+    #If statements to determine if guess is higher or lower than the random int
+    if guess < random_number:
+        print("Your guess is too low")
+    elif guess > random_number:
+        print("Your guess is too high")
+    #Exit the loop if number is correct
+    else:
+        print("Correct!")
+        break
+        
 
